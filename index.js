d23ba1cb3b9d746cf44cf183a4e26ab9902036bd
winstoncwang <congwang@live.co.uk>
2019-11-01 07:50:39 +0000

    Version 0.14
    autocomplete widget html container, html code generated using javascript

--- index.js
+++ index.js
@@ -13,19 +13,37 @@ const fetchData = async (searchTerm) => {
 	return response.data.Search;
 };
 
-const input = document.querySelector('input');
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+	<label><b>Search For a Movie</b></label>
+    <input class="input"/>
+	<div class="dropdown">
+        <div class="dropdown-menu">
+          <div class="dropdown-content result">
+          </div>
+        </div>
+      </div>
+	<div id="target"></div>
+`;
+
+const input = document.querySelector('.input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.result');
 
 const onInput = async (event) => {
 	const movies = await fetchData(event.target.value);
 
+	dropdown.classList.add('is-active');
 	for (let movie of movies) {
-		const div = document.createElement('div');
-		div.innerHTML = `
+		const option = document.createElement('a');
+
+		option.classList.add('dropdown-item');
+		option.innerHTML = `
 		<img src="${movie.Poster}" />
-		<h1>${movie.Title}</h1>
+		${movie.Title}
 		`;
 
-		document.querySelector('#target').appendChild(div);
+		resultsWrapper.appendChild(option);
 	}
 };
 
