597ddb83cb9be65cc9d28914fe498c34845e544a
winstoncwang <congwang@live.co.uk>
2019-11-04 16:38:48 +0000

    Version 0.161
    structure createAutoComplete to call the reusable codes from autocomplete.js

--- index.js
+++ index.js
@@ -12,58 +12,18 @@ const fetchData = async (searchTerm) => {
 	return response.data.Search;
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-	<label><b>Search For a Movie</b></label>
-    <input class="input"/>
-	<div class="dropdown">
-        <div class="dropdown-menu">
-          <div class="dropdown-content result">
-          </div>
-        </div>
-      </div>
-	<div id="target"></div>
-`;
-
-const input = document.querySelector('.input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.result');
-
-const onInput = async (event) => {
-	const movies = await fetchData(event.target.value);
-
-	if (!movies.length) {
-		dropdown.classList.remove('is-active');
-		return;
-	}
-
-	resultsWrapper.innerHTML = '';
-	for (let movie of movies) {
-		const option = document.createElement('a');
+createAutoComplete({
+	root           : document.querySelector('.autocomplete'),
+	renderOption(movie) {
 		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
-		option.classList.add('dropdown-item');
-		option.innerHTML = `
+		return `
 		<img src="${imgSrc}" />
 		${movie.Title}
 		`;
-
-		option.addEventListener('click', () => {
-			dropdown.classList.remove('is-active');
-			input.value = movie.Title; // this is added when each anchor is created. each anchor has eventlistener
-			onMovieSelect(movie);
-		});
-
-		resultsWrapper.appendChild(option);
-	}
-	dropdown.classList.add('is-active');
-};
-
-input.addEventListener('input', debounce(onInput, 500));
-
-document.addEventListener('click', (event) => {
-	if (!root.contains(event.target)) {
-		dropdown.classList.remove('is-active');
+	},
+	onOptionSelect(movie) {
+		onMovieSelect(movie);
 	}
 });
 
